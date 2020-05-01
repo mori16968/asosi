0db71904a1f42333b300747e99baab3cb835573b
Takahiko Nakahama <altair.vega16968@gmail.com>
2020-05-01 19:41:50 +0900

    createの前まで

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
